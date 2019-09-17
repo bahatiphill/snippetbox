34cdc1330dd62e73d09301def1e02da8798b2744
phill <phill@localhost.localdomain>
2019-09-17 15:09:02 +0200

    Code restructuring

---
+++ cmd/web/handlers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippet"))
+}
+
+// Displaying Specific Snippet Handler
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Displaying Specific snippet with ID %d..", id)
+}
+
+//Create a Snippet handler
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	w.Write([]byte("Creating Snippet"))
+}
