3b18271aae61e7b692479e2bcf15a0e523e1be49
phill <phill@localhost.localdomain>
2019-09-18 11:25:52 +0200

    Added Database pool connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //Define an application struct to hold the application-wide dependencies
@@ -17,11 +19,18 @@ func main() {
 
 	//Check if there is a "addr" flag passed on the command line and use it as port number
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	//command-line flag for MySQL DSN
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
-
 	//creating loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	//Initialize a new instance of application containing dependencies
 	app := &application{
@@ -37,6 +46,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
+
+//The openDB function wrap slq.Open() and return a sql.Db connection pool
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+} 
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module snippetbox-modules
+
+require github.com/go-sql-driver/mysql v1.4.1 // indirect
