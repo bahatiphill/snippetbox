368c680f019deee8e0bcdc25b1788677d5eab533
phill <phill@localhost.localdomain>
2019-09-19 10:24:42 +0200

    cathing runtime error during template rendering

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 //ServerError helper writes an error and stack trace to the errorLog
@@ -23,6 +25,15 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
+//this take a pinter to a templateData struct, adds the current year then return the pointer
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 
 	//retrieve the appropriate template set from the cache based on the page name
@@ -31,9 +42,14 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 		app.serverError(w, fmt.Errorf("The template %s does not exist", name))
 		return
 	}
-	//Execute the template set, passing in any dynamic data
-	err := ts.Execute(w, td)
+	//initialize a buffer
+	buf := new(bytes.Buffer)
+
+	//Write the template to the buffer instead of straight to the http.ResponseWriter
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 	}
+	//write the contents of buffer to the http.responseWriter
+	buf.WriteTo(w)
 }
